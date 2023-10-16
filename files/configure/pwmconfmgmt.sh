0567eb98b19d343b77b392dca1033091f289c1d3
dalerichardson <dalerichardson@users.noreply.github.com>
2023-10-16 09:09:32 -0600

    update to pass shellcheck

--- files/configure/pwmconfmgmt.sh
+++ files/configure/pwmconfmgmt.sh
@@ -10,9 +10,9 @@ sed 's/.*\///' /tmp/sha1conf >> /tmp/PwmConfiguration.xml.sha1
 sed -i ':a;N;$!ba;s/\n/\ /g' /tmp/PwmConfiguration.xml.sha1
 rm -rf /tmp/sha1conf
 logger "created sha1file in tmp"
-# shellcheck disable=SC1083,SC1036
+# shellcheck disable=SC1083,SC1036,SC1088
 aws s3 cp /usr/share/tomcat/webapps/ROOT/WEB-INF/PwmConfiguration.xml s3://{{ salt.pillar.get('pwm:lookup:config_bucket') }}/PwmConfiguration.xml
 logger "s3 put conf.xml file"
-# shellcheck disable=SC1036,SC1083
+# shellcheck disable=SC1036,SC1083,SC1088
 aws s3 cp /tmp/PwmConfiguration.xml.sha1 s3://{{ salt.pillar.get('pwm:lookup:config_bucket') }}/PwmConfiguration.xml.sha1 --acl public-read
 logger "s3 put conffile sha1"
